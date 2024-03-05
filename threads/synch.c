6c34e49c0b877f6be492c800b92b38a508323993
Ruka732 <ruka732@gmail.com>
2024-03-05 13:46:41 +0000

    priority-condvar, sema

--- threads/synch.c
+++ threads/synch.c
@@ -111,9 +111,11 @@ sema_up (struct semaphore *sema) {
 	ASSERT (sema != NULL);
 
 	old_level = intr_disable ();
-	if (!list_empty (&sema->waiters))
+	if (!list_empty (&sema->waiters)){
+      list_sort(&sema->waiters, cmp_priority, NULL);
 		thread_unblock (list_entry (list_pop_front (&sema->waiters),
 					struct thread, elem));
+   }
 	sema->value++;
 
 	thread_preemption();
